b1f0329796d2ca929c91a004a6c2d3e851acce72
Alejandro Duque <ducode@outlook.com>
2025-09-17 00:27:53 -0500

    Refactor footer social links in index.php
    - Commented out unused social media links for cleaner code.
    - Updated Google+ and Instagram links with new user information for better accuracy.

--- noticias/index.php
+++ noticias/index.php
@@ -950,7 +950,7 @@ $current_tournament = !empty($active_tournaments) ? $active_tournaments[0] : nul
 			<div class="footer-social">
 				<div class="container">
 					<ul class="footer-social__list list-unstyled">
-						<li class="footer-social__item">
+						<!-- <li class="footer-social__item">
 							<a href="#" class="footer-social__link">
 								<span class="footer-social__icon">
 									<i class="fab fa-facebook"></i>
@@ -960,59 +960,28 @@ $current_tournament = !empty($active_tournaments) ? $active_tournaments[0] : nul
 									<span class="footer-social__user">/alchemistsgaming</span>
 								</div>
 							</a>
-						</li>
+						</li> -->
+	
+			
 						<li class="footer-social__item">
-							<a href="#" class="footer-social__link">
-								<span class="footer-social__icon">
-									<i class="fab fa-twitter"></i>
-								</span>
-								<div class="footer-social__txt">
-									<span class="footer-social__name">Twitter</span>
-									<span class="footer-social__user">@alchemistsgaming</span>
-								</div>
-							</a>
-						</li>
-						<li class="footer-social__item">
-							<a href="#" class="footer-social__link">
-								<span class="footer-social__icon">
-									<i class="fab fa-twitch"></i>
-								</span>
-								<div class="footer-social__txt">
-									<span class="footer-social__name">Twitch</span>
-									<span class="footer-social__user">@alchemistsgaming</span>
-								</div>
-							</a>
-						</li>
-						<li class="footer-social__item">
-							<a href="#" class="footer-social__link">
-								<span class="footer-social__icon">
-									<i class="fab fa-youtube"></i>
-								</span>
-								<div class="footer-social__txt">
-									<span class="footer-social__name">YouTube</span>
-									<span class="footer-social__user">@alchemistsgaming</span>
-								</div>
-							</a>
-						</li>
-						<li class="footer-social__item">
-							<a href="#" class="footer-social__link">
+							<a href="mailto:abedeport@gmail.com" class="footer-social__link">
 								<span class="footer-social__icon">
 									<i class="fab fa-google-plus-g"></i>
 								</span>
 								<div class="footer-social__txt">
 									<span class="footer-social__name">Google+</span>
-									<span class="footer-social__user">/alchemistsgaming</span>
+									<span class="footer-social__user">/abedeport</span>
 								</div>
 							</a>
 						</li>
 						<li class="footer-social__item">
-							<a href="#" class="footer-social__link">
+							<a href="https://www.instagram.com/abede.port" class="footer-social__link">
 								<span class="footer-social__icon">
 									<i class="fab fa-instagram"></i>
 								</span>
 								<div class="footer-social__txt">
 									<span class="footer-social__name">Instagram</span>
-									<span class="footer-social__user">@alchemistsgaming</span>
+									<span class="footer-social__user">@abede.port</span>
 								</div>
 							</a>
 						</li>
